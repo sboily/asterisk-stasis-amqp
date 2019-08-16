5c8b2118a171e137ae3b2ed6d8e5b4a239370732
Nicolaos Ballas <nballas@wazo.io>
2019-08-16 15:15:47 -0400

    added ami

--- res_stasis_amqp.c
+++ res_stasis_amqp.c
@@ -725,8 +725,10 @@ int ast_unsubscribe_from_stasis(const char *app_name)
 	return 0;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "Send all Stasis messages to AMQP",
-	.support_level = AST_MODULE_SUPPORT_EXTENDED,
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS | AST_MODFLAG_LOAD_ORDER, "Send all Stasis messages to AMQP",
+	.support_level = AST_MODULE_SUPPORT_CORE,
 	.load = load_module,
-	.unload = unload_module
+	.unload = unload_module,
+	.load_pri = AST_MODPRI_APP_DEPEND,
+	.requires = "res_stasis,res_amqp",
 );
